7c81d7f57a483212ef1267128be9110b1bb44a19
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-18 08:26:35 +0000

    Deploy intranda/goobi-viewer-core to github.com/intranda/goobi-viewer-core.git:gh-pages

--- goobi-viewer-core/src/de/intranda/digiverso/presentation/Version.java
+++ goobi-viewer-core/src/de/intranda/digiverso/presentation/Version.java
@@ -6,7 +6,7 @@
 			*/
 			public class Version {
 			public final static String VERSION = "Goobi viewer-3.2";
-			public final static String BUILDVERSION = "df382ba";
+			public final static String BUILDVERSION = "0a6194f";
 			public final static String BUILDDATE = "20181218";
 			}
 		
